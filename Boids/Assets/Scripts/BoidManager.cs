c02b06285ccf27f3b7f67025e5c87948f2f70b2d
DanyDiab <danydiab04@gmail.com>
2026-02-15 22:45:16 -0400

    created boid object pool for spawniing

--- Boids/Assets/Scripts/BoidManager.cs
+++ Boids/Assets/Scripts/BoidManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,16 +7,19 @@ public class BoidManager : MonoBehaviour{
     GameObject[] boids;
     [SerializeField] GameObject boidPrefab;
     [SerializeField] GameObject simulationBounds;
+
+    List<GameObject> boidPool;
     float width;
     float height;
     void Start(){
         numBoids = 10;
         boids = new GameObject[numBoids];
+        boidPool = new List<GameObject>();
         Vector3 simScale = simulationBounds.transform.localScale;
         width = simScale.x;
         height = simScale.y;
+        spawnBoids();
     }
-
 
     void Update(){
         if(Keyboard.current == null) return;
@@ -26,27 +30,50 @@ public class BoidManager : MonoBehaviour{
         }
     }
 
-    void spawnBoids() {
+    (Vector3, Quaternion) getRandPosRot() {
         float halfWidth = width / 2;
         float halfHeight = height / 2;
-        for(int i = 0; i < numBoids; i++) {
-            float posX = halfWidth - Random.Range(0,width);
-            float posZ = halfHeight - Random.Range(0,height);
-            float randRot = Random.Range(0,359);
-            Vector3 pos = new Vector3(posX,1,posZ);
-            Quaternion rotation = Quaternion.Euler(0,randRot,0);
-            GameObject boid = Instantiate(boidPrefab,pos,rotation);
+        float posX = halfWidth - Random.Range(0,width);
+        float posZ = halfHeight - Random.Range(0,height);
+        float randRot = Random.Range(0,359);
+        Vector3 pos = new Vector3(posX,1,posZ);
+        Quaternion rotation = Quaternion.Euler(0,randRot,0);
+        return(pos,rotation);
+    }
+
+    void spawnBoids() {
+
+        int poolCount = boidPool.Count;
+        int maxGrab = Mathf.Min(numBoids,poolCount);
+
+        if(maxGrab > 0) {
+            int lastElement = boidPool.Count - 1;
+            for(int i = 0; i < maxGrab; i++) {
+                GameObject boid = boidPool[lastElement];
+                boid.SetActive(true);
+                boidPool.RemoveAt(lastElement);
+                boids[i] = boid;
+                (Vector3 pos, Quaternion rot) = getRandPosRot();
+                Transform boidTrans = boid.transform;
+                boidTrans.position = pos;
+                boidTrans.rotation = rot;
+                lastElement--;
+            }
+        }
+
+        for(int i = maxGrab; i < numBoids; i++) {
+            (Vector3 pos, Quaternion rot) = getRandPosRot();
+            GameObject boid = Instantiate(boidPrefab,pos,rot);
             boids[i] = boid;
         }
+
     }
 
     void clearBoids() {
+        boidPool.AddRange(boids);
         for(int i = 0; i < numBoids; i++) {
-            GameObject boid = boids[i];
-            Destroy(boid);
+            boids[i].SetActive(false);      
             boids[i] = null;
         }
-    }
-
-
+    }    
 }
