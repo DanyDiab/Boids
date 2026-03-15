a682a8a6ccc1383cb04f454b1a59259e79458482
DanyDiab <danydiab04@gmail.com>
2026-03-15 20:05:41 -0300

    quadtree neighbor search working, needs optimization

--- Boids/Assets/Scripts/SearchAlgos/QuadTreeSearch.cs
+++ Boids/Assets/Scripts/SearchAlgos/QuadTreeSearch.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 // -1 for no child
 
@@ -9,139 +10,213 @@ using UnityEngine;
 // For more performance, should be swapped to flat arrays
 namespace QuadTree {
     
-public class QuadTreeSearch : IBoidSearch {
-    Boid[] boids;
-    Vector3[] boidPositions;
-    int leafCapacity;
-    float simBoundRadius;
-    List<Node> nodes;
-    SimulationParameters simParams;
-    int numBoids;
+    public class QuadTreeSearch : IBoidSearch {
+        Boid[] boids;
+        Vector3[] boidPositions;
+        int leafCapacity;
+        float simBoundRadius;
+        List<Node> nodes;
+        SimulationParameters simParams;
+        int numBoids;
+        Boid[] currNeighbors;
+        int quadrentChecks;
 
-    public QuadTreeSearch(int numBoids, int leafCapacity, float simBoundRadius, SimulationParameters simParams) {
-        boids = new Boid[numBoids];
-        boidPositions = new Vector3[numBoids];
-        nodes = new List<Node>();
-        this.leafCapacity = leafCapacity;
-        this.simBoundRadius = simBoundRadius;
-        this.simParams = simParams;
-        Node node = new Node(-1);
-        nodes.Add(node);
-        this.numBoids = simParams.NumBoids;
-    }
-
-    public void AddBoid(int index, Vector3 position, Boid boid) {
-        boidPositions[index] = position;
-        boids[index] = boid;
-        addBoidToTree(position, index);
-
-    }
-
-
-    void addBoidToTree(Vector3 position, int index) {
-        Vector2 startingBoxDims = new Vector2(simBoundRadius,simBoundRadius);
-        findLeafRecur(position, 1, startingBoxDims, new Vector2(-simBoundRadius / 2,-simBoundRadius / 2), 0, index);
-    }
-    
-    void findLeafRecur(Vector3 pos, int depth, Vector2 boxDims, Vector2 offset, int currNodeIndex, int index) {
-        if (!isPointInSquare(pos, boxDims, offset)) {
-            return;
+        public QuadTreeSearch(int numBoids, int leafCapacity, float simBoundRadius, SimulationParameters simParams) {
+            boids = new Boid[numBoids];
+            boidPositions = new Vector3[numBoids];
+            nodes = new List<Node>();
+            this.leafCapacity = leafCapacity;
+            this.simBoundRadius = simBoundRadius;
+            this.simParams = simParams;
+            Node node = new Node(-1);
+            nodes.Add(node);
+            this.numBoids = simParams.NumBoids;
+            currNeighbors = new Boid[numBoids - 1];
+            quadrentChecks = 0;
         }
-        float width = boxDims.x / 2;
-        float height = boxDims.y / 2;
-        Vector2 newBoxDims = new Vector2(width,height);
 
-        Vector2 tl = offset;
-        Vector2 tr = new Vector2(offset.x + width, offset.y);
-        Vector2 bl = new Vector2(offset.x, offset.y + height);
-        Vector2 br = new Vector2(offset.x + width, offset.y + height);
+        public void AddBoid(int index, Vector3 position, Boid boid) {
+            boidPositions[index] = position;
+            boids[index] = boid;
+            addBoidToTree(position, index);
 
-        // leaf node
-        Node currNode = nodes[currNodeIndex];
-        if(currNode.FirstChild == -1) {
-            currNode.BoidIDs.Add(index);
-            if(currNode.BoidIDs.Count <= leafCapacity) {
+        }
+        
+        void addBoidToTree(Vector3 position, int index) {
+            Vector2 startingBoxDims = new Vector2(simBoundRadius,simBoundRadius);
+            findLeafRecur(position, 1, startingBoxDims, new Vector2(-simBoundRadius / 2,-simBoundRadius / 2), 0, index);
+        }
+        
+        void findLeafRecur(Vector3 pos, int depth, Vector2 boxDims, Vector2 offset, int currNodeIndex, int index) {
+            if (!isPointInSquare(pos, boxDims, offset)) {
+                return;
+            }
+            float width = boxDims.x / 2;
+            float height = boxDims.y / 2;
+            Vector2 newBoxDims = new Vector2(width,height);
+
+            Vector2 tl = offset;
+            Vector2 tr = new Vector2(offset.x + width, offset.y);
+            Vector2 bl = new Vector2(offset.x, offset.y + height);
+            Vector2 br = new Vector2(offset.x + width, offset.y + height);
+
+            // leaf node
+            Node currNode = nodes[currNodeIndex];
+            if(currNode.FirstChild == -1) {
+                currNode.BoidIDs.Add(index);
+                if(currNode.BoidIDs.Count <= leafCapacity) {
+                    nodes[currNodeIndex] = currNode;
+                    return;
+                }
+                // split
+                
+                Node newtlNode = new Node(-1);
+                Node newtrNode = new Node(-1);
+                Node newblNode = new Node(-1);
+                Node newbrNode = new Node(-1);
+
+                currNode.FirstChild = nodes.Count;
+
+                nodes.Add(newtlNode);
+                nodes.Add(newtrNode);
+                nodes.Add(newblNode);
+                nodes.Add(newbrNode);
+
+                // redistribute boids
+                foreach(int boidID in currNode.BoidIDs) {
+                    Vector3 boidPos = boidPositions[boidID];
+                    if (isPointInSquare(boidPos, newBoxDims, tl)) {
+                        newtlNode.BoidIDs.Add(boidID);
+                    }
+                    if (isPointInSquare(boidPos, newBoxDims, tr)) {
+                        newtrNode.BoidIDs.Add(boidID);
+                    }
+                    if (isPointInSquare(boidPos, newBoxDims, bl)) {
+                        newblNode.BoidIDs.Add(boidID);
+                    }
+                    if (isPointInSquare(boidPos, newBoxDims, br)) {
+                        newbrNode.BoidIDs.Add(boidID);
+                    }
+                }
+                currNode.BoidIDs.Clear();
                 nodes[currNodeIndex] = currNode;
                 return;
             }
-            // split
             
-            Node newtlNode = new Node(-1);
-            Node newtrNode = new Node(-1);
-            Node newblNode = new Node(-1);
-            Node newbrNode = new Node(-1);
+            // branch recursive step
+            int firstChild = currNode.FirstChild;
 
-            currNode.FirstChild = nodes.Count;
+            findLeafRecur(pos,depth + 1,newBoxDims,tl,firstChild,index);
+            findLeafRecur(pos,depth + 1,newBoxDims,tr,firstChild + 1,index);
+            findLeafRecur(pos,depth + 1,newBoxDims,bl,firstChild + 2,index);
+            findLeafRecur(pos,depth + 1,newBoxDims,br,firstChild + 3,index);
+        }
 
-            nodes.Add(newtlNode);
-            nodes.Add(newtrNode);
-            nodes.Add(newblNode);
-            nodes.Add(newbrNode);
+        bool isPointInSquare(Vector3 pos, Vector2 boxDims, Vector2 offset) {
+            Vector2 pos2D = new Vector2(pos.x,pos.z);
 
-            // redistribute boids
-            foreach(int boidID in currNode.BoidIDs) {
-                Vector3 boidPos = boidPositions[boidID];
-                if (isPointInSquare(boidPos, newBoxDims, tl)) {
-                    newtlNode.BoidIDs.Add(boidID);
+            bool xGood = pos2D.x >= offset.x && pos2D.x < offset.x + boxDims.x;
+            bool zGood = pos2D.y >= offset.y && pos2D.y < offset.y + boxDims.y;
+
+            return xGood && zGood;
+        }
+
+        void buildQuadTree() {
+            nodes.Clear();
+            Node node = new Node(-1);
+            nodes.Add(node);
+            for(int i = 0; i < numBoids; i++) {
+                Vector3 pos = boidPositions[i];
+                addBoidToTree(pos,i);
+            }
+            simParams.Nodes = nodes;
+        }
+
+        public void RemoveBoid(int index) {
+            boids[index] = null;
+        }
+
+        public void UpdatePosition(int index, Vector3 position) {
+            boidPositions[index] = position;
+        }
+
+        bool IsBoidTouchingQuadrent(Vector3 position, float radius, Vector2 boxDims, Vector2 min) {
+            quadrentChecks++;
+            Vector2 pos2 = new Vector2(position.x,position.z);
+            Vector2 max = min + boxDims;
+
+            // find closest point on quadrent to boid
+            float cx = Mathf.Clamp(pos2.x,min.x,max.x);
+            float cy = Mathf.Clamp(pos2.y,min.y,max.y);
+
+            Vector2 clamped = new Vector2(cx,cy);
+            float distance = (pos2 - clamped).magnitude;
+            if(distance * distance < radius * radius) {
+                return true;
+            }
+            return false;
+        }
+
+        void FindTouchingLeafs(Vector3 position, float radius, int nodeIndex, Vector2 boxDims, Vector2 offset, List<int> result) {
+            if (!IsBoidTouchingQuadrent(position, radius, boxDims, offset)) {
+                return;
+            }
+            Node currNode = nodes[nodeIndex];
+            int firstChild = currNode.FirstChild;
+            // branch
+            Vector2 newDims = new Vector2(boxDims.x / 2, boxDims.y / 2);
+            if(firstChild != -1) {
+                Vector2 tl = offset;
+                Vector2 tr = new Vector2(offset.x + newDims.x, offset.y);
+                Vector2 bl = new Vector2(offset.x, offset.y + newDims.y);
+                Vector2 br = new Vector2(offset.x + newDims.x, offset.y + newDims.y);
+
+                if (IsBoidTouchingQuadrent(position, radius, newDims, tl)) {
+                    FindTouchingLeafs(position,radius,firstChild,newDims,tl, result);
                 }
-                if (isPointInSquare(boidPos, newBoxDims, tr)) {
-                    newtrNode.BoidIDs.Add(boidID);
+                if (IsBoidTouchingQuadrent(position, radius, newDims, tr)) {
+                    FindTouchingLeafs(position,radius,firstChild + 1,newDims,tr,result);
                 }
-                if (isPointInSquare(boidPos, newBoxDims, bl)) {
-                    newblNode.BoidIDs.Add(boidID);
+                if (IsBoidTouchingQuadrent(position, radius, newDims, bl)) {
+                    FindTouchingLeafs(position,radius,firstChild + 2,newDims,bl,result);
                 }
-                if (isPointInSquare(boidPos, newBoxDims, br)) {
-                    newbrNode.BoidIDs.Add(boidID);
+                if (IsBoidTouchingQuadrent(position, radius, newDims, br)) {
+                    FindTouchingLeafs(position,radius,firstChild + 3,newDims, br,result);
+                }
+                return;
+            }
+            // leaf
+            else {
+                result.AddRange(currNode.BoidIDs);
+            }
+        }
+
+        public (int numNeighbors, int numChecks, Boid[] neighbors) FindNeighbors(int index, float radius) {
+            if(index == 0) {
+                buildQuadTree();
+            }
+            Vector3 position = boidPositions[index];
+            Vector2 boxDims = new Vector2(simBoundRadius,simBoundRadius);
+            List<int> result = new List<int>();
+            FindTouchingLeafs(position,radius,0,boxDims,new Vector2(-simBoundRadius / 2,-simBoundRadius / 2),result);
+
+            int numNeighbors = 0;
+            int numChecks = 0;
+            int count = result.Count;
+            for(int i = 0; i < count; i++) {
+                int currBoidID = result[i];
+                if(currBoidID == index) continue;
+                numChecks++;
+                Boid currBoid = boids[currBoidID];
+                Vector3 currBoidPos = boidPositions[currBoidID];
+
+                float dist = (currBoidPos - position).magnitude;
+                if(dist * dist < radius * radius) {
+                    currNeighbors[numNeighbors] = currBoid;
+                    numNeighbors++;
                 }
             }
-            currNode.BoidIDs.Clear();
-            nodes[currNodeIndex] = currNode;
-            return;
+            return (numNeighbors,numChecks,currNeighbors);
         }
-        
-        // branch recursive step
-        int firstChild = currNode.FirstChild;
-
-        findLeafRecur(pos,depth + 1,newBoxDims,tl,firstChild,index);
-        findLeafRecur(pos,depth + 1,newBoxDims,tr,firstChild + 1,index);
-        findLeafRecur(pos,depth + 1,newBoxDims,bl,firstChild + 2,index);
-        findLeafRecur(pos,depth + 1,newBoxDims,br,firstChild + 3,index);
     }
-
-    bool isPointInSquare(Vector3 pos, Vector2 boxDims, Vector2 offset) {
-        Vector2 pos2D = new Vector2(pos.x,pos.z);
-
-        bool xGood = pos2D.x >= offset.x && pos2D.x < offset.x + boxDims.x;
-        bool zGood = pos2D.y >= offset.y && pos2D.y < offset.y + boxDims.y;
-
-        return xGood && zGood;
-    }
-
-    void buildQuadTree() {
-        nodes.Clear();
-        Node node = new Node(-1);
-        nodes.Add(node);
-        for(int i = 0; i < numBoids; i++) {
-            Vector3 pos = boidPositions[i];
-            addBoidToTree(pos,i);
-        }
-        simParams.Nodes = nodes;
-    }
-
-    public void RemoveBoid(int index) {
-        boids[index] = null;
-    }
-
-    public void UpdatePosition(int index, Vector3 position) {
-        boidPositions[index] = position;
-    }
-
-    public (int numNeighbors, int numChecks, Boid[] neighbors) FindNeighbors(int index, float radius) {
-        if(index == 0) {
-            buildQuadTree();
-        }
-        return (0,0,null);
-    }
-}
-
 }
