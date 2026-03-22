bcf4e3ac338d13fd05e80fd1015c14f9227200e8
DanyDiab <danydiab04@gmail.com>
2026-03-22 17:38:49 -0300

    have performance graphs created

--- Boids/Assets/Scripts/Helpers/GraphMaker.cs
+++ Boids/Assets/Scripts/Helpers/GraphMaker.cs
@@ -8,7 +8,9 @@ using System.Globalization;
 using System.IO;
 
 public class Graph : MonoBehaviour{
+    [Header("Charts")]
     [SerializeField] BarChart barChart;
+    [SerializeField] ScatterChart scatterChart;
     string resultsPath;
     string fileDir;
     List<ExperimentRecord> experimentRecords;
@@ -17,7 +19,8 @@ public class Graph : MonoBehaviour{
         fileDir = Application.dataPath + "/Scripts/Results";
         resultsPath = $"{fileDir}/ExperimentResults.csv";
         experimentRecords = getCsvData();
-        generatePerformanceChart(barChart, "Optimal Averages", "Averages across the optimal settings for various map sizes");
+        generatePerformanceBar(barChart, "Optimal Averages", "Averages across the optimal settings for various map sizes");
+        generatePerformanceScatter(scatterChart, "Scatter Chart", "TEST");
     }
 
     List<ExperimentRecord> getCsvData() {
@@ -28,12 +31,23 @@ public class Graph : MonoBehaviour{
         }
         return records;
     }
-    void generatePerformanceChart(BarChart barChart, string title, string subTitle) {
-        var titleComponent = barChart.EnsureChartComponent<Title>();
+
+
+    void addTitleToChart(BaseChart chart, string title, string subTitle){
+        var titleComponent = chart.EnsureChartComponent<Title>();
         titleComponent.show = true;
         titleComponent.text = title;
         titleComponent.subText = subTitle;
+    }
+
+    void enableLegend(BaseChart chart) {
+        Legend legend = chart.EnsureChartComponent<Legend>();
+        legend.show = true;
+        legend.location.align = Location.Align.TopRight;
+    }
+    void generatePerformanceBar(BarChart barChart, string title, string subTitle) {
         barChart.ClearData();
+        addTitleToChart(barChart,title,subTitle);
         var optimalResults = experimentRecords
             .GroupBy(run => new { run.searchAlgo, run.mapSize })
             .Select(group => new {
@@ -52,6 +66,42 @@ public class Graph : MonoBehaviour{
         foreach(var avg in optimalResults) {
             barChart.AddXAxisData("" + avg.Algorithm);
             barChart.AddData(0, avg.AverageOptimalMS);
+        }
+    }
+
+
+
+    void generatePerformanceScatter(ScatterChart chart, string title, string subTitle) {
+        addTitleToChart(chart, title, subTitle);
+        enableLegend(chart);
+        
+        Serie uniformGrid = chart.GetSerie(0);
+        uniformGrid.serieName = "Uniform Grid";
+        
+        Serie quadTree = chart.GetSerie(1);
+        quadTree.serieName = "Quadtree";
+
+        XAxis xAxis = chart.EnsureChartComponent<XAxis>();
+        xAxis.type = Axis.AxisType.Value;
+        xAxis.axisName.show = true;
+        xAxis.axisName.name = "Average Density";
+
+        YAxis yAxis = chart.EnsureChartComponent<YAxis>();
+        yAxis.type = Axis.AxisType.Value;
+        yAxis.axisName.show = true;
+        yAxis.axisName.name = "Average Total MS";
+        
+        Tooltip tooltip = chart.EnsureChartComponent<Tooltip>();
+        tooltip.show = true;
+        
+        tooltip.itemFormatter = "{a} <br/> Parameter Size: {c2} <br/> Density: {c0:f2} <br/> Total MS: {c1:f2}";
+        
+        chart.ClearData();
+        
+        foreach(ExperimentRecord record in experimentRecords) {
+            int seriesIndex = record.searchAlgo == SearchAlgos.UNIFORMGRID ? 0 : 1;
+            
+            chart.AddData(seriesIndex, record.averageDensity, record.averageTotalMS, record.leafCapacityOrCellSize);
         }
     }
 }
